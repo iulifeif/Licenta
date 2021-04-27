452b02403b31651a53a1aae85d240d978377ff57
Iuliana Holban <iulifeif@gmail.com>
2021-04-27 18:19:24 +0300

    create backend

---
+++ backend/src/utils/exceptions.py
@@ -0,0 +1,12 @@
+class Conflict(Exception):
+    def __init__(self, *args, **kwargs):
+        self.status = kwargs["status"]
+        del kwargs["status"]
+        super().__init__(*args, **kwargs)
+
+
+class InvalidBody(Exception):
+    def __init__(self, *args, **kwargs):
+        self.status = kwargs["status"]
+        del kwargs["status"]
+        super().__init__(*args, **kwargs)
